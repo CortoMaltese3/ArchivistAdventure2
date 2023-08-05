952cec68c1133f19833fc3e524e852807fe36c17
CortoMaltese3 <giorgos.kalomalos@outlook.com>
2023-08-05 22:55:55 +0300

    chore/Refactor COLOR_MAP dictionary
    - Restructure the COLOR_MAP dict to assign different objects to colors and numerical values.
    - Refactor save_grid method to create separate files per entity

--- map_generator/map_gen.py
+++ map_generator/map_gen.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import tkinter as tk
 import pandas as pd
 
@@ -5,30 +7,6 @@ import settings
 
 
 class Grid:
-    """
-    Class that provides a grid of cells in a Tkinter window, allowing
-    interactive coloring of cells that can be saved to a CSV file.
-
-    ...
-
-    Attributes
-    ----------
-    rows : int
-        Number of rows in the grid
-    cols : int
-        Number of columns in the grid
-    cell_size : int
-        Size of each cell in pixels
-    grid : List[List[int]]
-        2D list representing the grid state
-    color_map : Dict[str, int]
-        Mapping from color names to integer values
-    current_color : str
-        Current color selected for drawing
-    canvas : tk.Canvas
-        The Tkinter Canvas for drawing
-    """
-
     def __init__(
         self,
         master,
@@ -36,26 +14,14 @@ class Grid:
         cols=settings.COLS,
         cell_size=settings.CELL_SIZE,
     ):
-        """
-        Creates a new Grid instance.
-
-        Parameters
-        ----------
-        master : tk.Root
-            The root Tkinter window
-        rows : int, optional
-            Number of rows in the grid, by default 10
-        cols : int, optional
-            Number of columns in the grid, by default 10
-        cell_size : int, optional
-            Size of each cell in pixels, by default 20
-        """
         self.rows = rows
         self.cols = cols
         self.cell_size = cell_size
-        self.grid = [[0] * cols for _ in range(rows)]
+        self.grid = [
+            [settings.COLOR_MAP.get("air")[1]] * cols for _ in range(rows)
+        ]  # using 'air' instead of 'white'
         self.color_map = settings.COLOR_MAP
-        self.current_color = "black"
+        self.current_color = "wall"
         self.canvas = tk.Canvas(
             master, width=self.cols * self.cell_size, height=self.rows * self.cell_size
         )
@@ -69,75 +35,51 @@ class Grid:
         self.canvas.bind("<B1-Motion>", self.click)
 
     def click(self, event):
-        """
-        Event handler for mouse clicks and drags. Updates the clicked cell's
-        color and redraws it.
-
-        Parameters
-        ----------
-        event : tk.Event
-            The event information
-        """
         row = event.y // self.cell_size
         col = event.x // self.cell_size
         if 0 <= row < self.rows and 0 <= col < self.cols:
-            self.grid[row][col] = self.color_map[self.current_color]
+            self.grid[row][col] = self.color_map[self.current_color][
+                1
+            ]  # use the numerical code from COLOR_MAP
             self.draw_cell(row, col)
 
     def draw_cell(self, row, col):
-        """
-        Draws a cell on the canvas with the color corresponding to its
-        current state.
-
-        Parameters
-        ----------
-        row : int
-            Row number of the cell
-        col : int
-            Column number of the cell
-        """
         x1 = col * self.cell_size
         y1 = row * self.cell_size
         x2 = x1 + self.cell_size
         y2 = y1 + self.cell_size
-        color = list(self.color_map.keys())[
-            list(self.color_map.values()).index(self.grid[row][col])
-        ]
+        color = [
+            v[0] for k, v in self.color_map.items() if v[1] == self.grid[row][col]
+        ][0]
         self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, width=1)
 
     def save_grid(self):
-        """Saves the current state of the grid to a CSV file."""
+        """Saves the current state of the grid to multiple CSV files."""
         df = pd.DataFrame(self.grid)
-        df.to_csv(settings.MAP_FLOOR_BLOCKS, index=False, header=False)
+
+        air_num = settings.COLOR_MAP['air'][1]
+
+        # Mapping of entities to their numerical codes for each category
+        map_floor_blocks = {v[1]: v[1] for k, v in settings.COLOR_MAP.items() if k in {"air", "wall"}}
+        map_entities = {v[1]: v[1] for k, v in settings.COLOR_MAP.items() if k in {"air", "player", "enemy"}}
+        map_grass = {v[1]: v[1] for k, v in settings.COLOR_MAP.items() if k in {"air", "grass"}}
+        map_objects = {v[1]: v[1] for k, v in settings.COLOR_MAP.items() if k in {"air", "object"}}
+
+        # Create copies of the DataFrame for each category and replace values not in the category with 'air'
+        df_floor_blocks = df.copy().replace({k: air_num for k in set(df.values.flatten()) - set(map_floor_blocks.keys())})
+        df_entities = df.copy().replace({k: air_num for k in set(df.values.flatten()) - set(map_entities.keys())})
+        df_grass = df.copy().replace({k: air_num for k in set(df.values.flatten()) - set(map_grass.keys())})
+        df_objects = df.copy().replace({k: air_num for k in set(df.values.flatten()) - set(map_objects.keys())})
+
+        # Save the modified DataFrames to CSV files
+        df_floor_blocks.to_csv(settings.MAP_FLOOR_BLOCKS, index=False, header=False)
+        df_entities.to_csv(settings.MAP_ENTITIES, index=False, header=False)
+        df_grass.to_csv(settings.MAP_GRASS, index=False, header=False)
+        df_objects.to_csv(settings.MAP_OBJECTS, index=False, header=False)
 
 
 class ColorChooser:
-    """
-    Class that provides a dropdown menu for choosing a color in a Tkinter window.
-
-    ...
-
-    Attributes
-    ----------
-    grid : Grid
-        The grid on which to operate
-    variable : tk.StringVar
-        The variable for the dropdown menu
-    option_menu : tk.OptionMenu
-        The dropdown menu
-    """
-
     def __init__(self, master, grid):
-        """
-        Creates a new ColorChooser instance.
-
-        Parameters
-        ----------
-        master : tk.Root
-            The root Tkinter window
-        grid : Grid
-            The grid on which to operate
-        """
         self.grid = grid
         self.variable = tk.StringVar(master)
         self.variable.set(list(grid.color_map.keys())[0])  # default value
@@ -147,32 +89,14 @@ class ColorChooser:
         self.option_menu.pack()
 
     def choose_color(self, value):
-        """
-        Sets the current color of the grid to the chosen value.
-
-        Parameters
-        ----------
-        value : str
-            The chosen color value
-        """
         self.grid.current_color = value
 
 
 def key(event):
-    """
-    Event handler for keypresses. If 's' key is pressed, the current state of
-    the grid is saved to a CSV file.
-
-    Parameters
-    ----------
-    event : tk.Event
-        The event information
-    """
     if event.char == "s":
         g.save_grid()
 
 
-# Create and start the application
 root = tk.Tk()
 g = Grid(root)
 c = ColorChooser(root, g)
--- map_generator/settings.py
+++ map_generator/settings.py
@@ -5,11 +5,22 @@ ROWS = 50
 COLS = 50
 CELL_SIZE = 15
 
-# Default color map (Color name: numerical value)
-COLOR_MAP = {"white": -1, "black": 395, "red": 2, "green": 3, "blue": 4}
+COLOR_MAP = {
+    "air": ("white", -1),
+    "wall": ("black", 395),
+    "player": ("gold", 394),
+    "grass": ("green", 8),
+    "water": ("blue", 4),
+    "enemy": ("red", 390),
+    "object": ("brown", 3),
+}
 
 # Get the base path of the application
 BASE_PATH = Path(__file__).parent.parent
 
+# Maps
 MAP_PATH = BASE_PATH / "map"
 MAP_FLOOR_BLOCKS = MAP_PATH / "map_FloorBlocks.csv"
+MAP_ENTITIES = MAP_PATH / "map_Entities.csv"
+MAP_GRASS = MAP_PATH / "map_Grass.csv"
+MAP_OBJECTS = MAP_PATH / "map_Objects.csv"
